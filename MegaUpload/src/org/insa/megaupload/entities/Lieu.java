6aa0e1aece65f46414e77f19d98a4bcfcb119806
jmarchan <jmarchan@0ed138ed-9e3e-4e1a-b415-34bd3ada5812>
2012-01-24 10:49:59 +0000

    Trajets dans les lieux
    git-svn-id: https://www.etud.insa-toulouse.fr/svn/CredigeMegaUpload/trunk@7 0ed138ed-9e3e-4e1a-b415-34bd3ada5812

--- MegaUpload/src/org/insa/megaupload/entities/Lieu.java
+++ MegaUpload/src/org/insa/megaupload/entities/Lieu.java
@@ -1,25 +1,37 @@
 package org.insa.megaupload.entities;
 
+import java.util.ArrayList;
+
 import org.insa.megaupload.example.Context;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 
 public class Lieu {
-
 	private String nom;
 	private int coutHeberg;
 	private int risque;
 	private int tempsMiseEnPlace;
 	private int x;
 	private int y;
-	private Image img;
-	private Image imgH;
+	private ArrayList<Trajet> trajets;
+	
+	private Image img, imgH;
 
-	public Lieu(String nom, int coutHeberg, int risque, int tempsMiseEnPlace) {
+	public Lieu(String nom, int x, int y, int coutHeberg, int risque, int tempsMiseEnPlace) {
 		super();
 		this.nom = nom;
+		this.x = x;
+		this.y = y;
 		this.coutHeberg = coutHeberg;
 		this.risque = risque;
 		this.tempsMiseEnPlace = tempsMiseEnPlace;
+		this.trajets = new ArrayList<Trajet>();
+		try {
+			this.img = new Image("resources/img/point-bleu.png");
+			this.imgH = new Image("resources/img/point-orange.png");
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public String getNom() {
@@ -44,6 +56,20 @@ public class Lieu {
 		} else {
 			img.draw(x, y);
 		}
+	}
+
+	/**
+	 * @return the trajets
+	 */
+	public ArrayList<Trajet> getTrajets() {
+		return trajets;
+	}
+
+	/**
+	 * @param trajet the trajet to add
+	 */
+	public void addTrajet(Trajet trajet) {
+		this.trajets.add(trajet);
 	}
 
 }
