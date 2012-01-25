07f1fd1079391fe8b32353f572c30da172bbe6e7
jmarchan <jmarchan@0ed138ed-9e3e-4e1a-b415-34bd3ada5812>
2012-01-25 16:10:28 +0000

    Ecran de chargement
    git-svn-id: https://www.etud.insa-toulouse.fr/svn/CredigeMegaUpload/trunk@60 0ed138ed-9e3e-4e1a-b415-34bd3ada5812

--- MegaUpload/src/org/insa/megaupload/example/Main.java
+++ MegaUpload/src/org/insa/megaupload/example/Main.java
@@ -1,11 +1,9 @@
-/**
- * 
- */
 package org.insa.megaupload.example;
+
+import java.io.IOException;
 
 import org.insa.megaupload.entities.AgentFBI;
 import org.insa.megaupload.entities.Carte;
-import org.insa.megaupload.entities.Deplacement;
 import org.insa.megaupload.entities.Lieu;
 import org.insa.megaupload.entities.MegaPerso;
 import org.insa.megaupload.entities.Personnage;
@@ -20,19 +18,20 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Music;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.loading.DeferredResource;
+import org.newdawn.slick.loading.LoadingList;
 
 /**
  * @author fougeane
- *
+ * 
  */
 public class Main extends BasicGame {
-	private boolean mousePressed;
-	private int mouseX;
-	private int mouseY;
-	
+	private boolean started;
+	private Music music;
+	private Image loadingImg;
 	private int cptSoft;
-	
-	public Main () {
+
+	public Main() {
 		super("MegaUpload");
 	}
 
@@ -53,8 +52,12 @@ public class Main extends BasicGame {
 
 	@Override
 	public void init(GameContainer container) throws SlickException {
+		loadingImg = new Image("resources/img/megaupload-logo.png");
+		LoadingList.setDeferredLoading(true);
+		
 		Carte c = new Carte();
 		Context.setCarte(c);
+		music = new Music("resources/sound/megasong.ogg");
 		Lieu.setImages(new Image("resources/img/City-20px.png"), new Image("resources/img/City-30px.png"));
 		MegaPerso kim = new MegaPerso("Kim DotCom", (Lieu) c.getLieux().toArray()[27], new Image("resources/img/kim.png"));
 		Context.addPersonnage(kim);
@@ -63,91 +66,97 @@ public class Main extends BasicGame {
 		Context.addPersonnage(a);
 		a.poursuivre(kim);
 		Context.setSelectedPerso(kim);
-		
-		
-		Music music = new Music("resources/sound/megasong.ogg");
-		music.loop();
 	}
 
 	@Override
 	public void update(GameContainer container, int delta) throws SlickException {
-		
-		//toutes les secondes
-		if(++this.cptSoft%100 == 0){
-			//récupérer les gains des serveurs
-			Context.incCptThunes(ServeurRules.getRegleGainBase()*Context.getCptServeursOuverts());
-		}
-		
-		//System.out.println("Update: " + delta);
-		for (Personnage p : Context.getPersonnages()) {
-			p.update(delta);
-		}
-		
+		if (LoadingList.get().getRemainingResources() > 0) {
+			DeferredResource nextResource = LoadingList.get().getNext();
+			try {
+				nextResource.load();
+			} catch (IOException e) {
+				throw new SlickException("Failed to load: " + nextResource.getDescription(), e);
+			}
+		} else {
+			if (!started) {
+				started = true;
+				music.loop();
+			}
 
+			//toutes les secondes
+			if(++this.cptSoft%100 == 0){
+				//récupérer les gains des serveurs
+				Context.incCptThunes(ServeurRules.getRegleGainBase()*Context.getCptServeursOuverts());
+			}
+			
+			//System.out.println("Update: " + delta);
+			for (Personnage p : Context.getPersonnages()) {
+				p.update(delta);
+			}
+		}
 	}
+	public void render(GameContainer container, Graphics g)	throws SlickException {
+		if (!started) {
+			g.drawString("Loading...", 500, 100);
+			loadingImg.draw((container.getWidth() - loadingImg.getWidth()) / 2,
+					(container.getHeight() - loadingImg.getHeight()) / 2);
+		} else {
+			Context.getCarte().draw(g);
+			g.drawString("Welcome to MegaUpload!", 10, 50);
+			g.drawString("Argent \n" + Integer.toString(Context.getCptThunes()), 1200, 50);
+			g.drawString("Serveurs\n" + Integer.toString(Context.getCptServeursOuverts()), 1200, 100);
+			if (Context.getSelectedPerso() != null) {
+				g.drawString("Selected perso: "	+ Context.getSelectedPerso().getNom(), 10, 600);
+			}
+			if (Context.getSelectedLieu() != null) {
+				g.drawString("Selected lieu: " + Context.getSelectedLieu().getNom(), 10, 620);
+			}
+			g.setColor(Color.red);
+			
+			for (Personnage p : Context.getPersonnages()) {
+				p.draw();
+			}
 
-	public void render(GameContainer container, Graphics g) throws SlickException {
-		Context.getCarte().draw(g);
-		g.drawString("Welcome to MegaUpload!", 10, 50);
-		g.drawString("Argent \n" + Integer.toString(Context.getCptThunes()), 1200, 50);
-		g.drawString("Serveurs\n" + Integer.toString(Context.getCptServeursOuverts()), 1200, 100);
-		if (Context.getSelectedPerso() != null) {
-			g.drawString("Selected perso: " + Context.getSelectedPerso().getNom(), 10, 600);
-		}
-		if (Context.getSelectedLieu() != null) {
-			g.drawString("Selected lieu: " + Context.getSelectedLieu().getNom(), 10, 620);
-		}
-		g.setColor(Color.red);
-		//if (this.mousePressed) {
-			int r = 6;
-			g.fillOval(this.mouseX - r/2, this.mouseY - r/2, r, r);
-			g.drawString("Mouse pressed: " + this.mouseX + ", " + this.mouseY, 10, 75);
-		//}
-				
-		//render des particules pour les personnages
-		Personnage.getParticleSystem().render();
-		
-		for (Personnage p : Context.getPersonnages()) {
-			p.draw();
-		}
-		
-		for (Lieu l : Context.getCarte().getLieux()) {
-			for (Trajet t : l.getTrajets()) {
-				g.drawLine(t.getDepart().getX(), t.getDepart().getY(), t.getArrivee().getX(), t.getArrivee().getY());
+			for (Lieu l : Context.getCarte().getLieux()) {
+				for (Trajet t : l.getTrajets()) {
+					g.drawLine(t.getDepart().getX(), t.getDepart().getY(), t.getArrivee().getX(), t.getArrivee().getY());
+				}
 			}
 		}
 		
 		
 	}
-
+	
 	@Override
 	public void mouseMoved(int oldx, int oldy, int newx, int newy) {
 		Lieu hoveredLieu = null;
 		MegaPerso hoveredPerso = null;
-		
-		//recherche d'un lieu correspondant à la position de la souris
+
+		// recherche d'un lieu correspondant à la position de la souris
 		for (Lieu l : Context.getCarte().getLieux()) {
-			if (newx >= l.getX() - l.getWidth()/2 && newx <= l.getX() + l.getWidth()/2 &&
-					newy >= l.getY() - l.getHeight()/2 && newy <= l.getY() + l.getHeight()/2) {
+			if (newx >= l.getX() - l.getWidth() / 2
+					&& newx <= l.getX() + l.getWidth() / 2
+					&& newy >= l.getY() - l.getHeight() / 2
+					&& newy <= l.getY() + l.getHeight() / 2) {
 				hoveredLieu = l;
 				break;
 			}
 		}
-		
+
 		for (Personnage p : Context.getPersonnages()) {
 			if (p instanceof MegaPerso) {
 				int num = ((MegaPerso) p).getNum();
-				if (newx <= 100 && newy >= (num + 1)*100 && newy <= (num + 2)*100) {
+				if (newx <= 100 && newy >= (num + 1) * 100 && newy <= (num + 2) * 100) {
 					hoveredPerso = ((MegaPerso) p);
 					break;
 				}
 			}
 		}
-		
+
 		Context.setHoveredLieu(hoveredLieu);
 		Context.setHoveredPerso(hoveredPerso);
 	}
-
+	
 	@Override
 	public void mouseClicked(int button, int x, int y, int clickCount) {
 		if(clickCount == 1){
@@ -197,17 +206,4 @@ public class Main extends BasicGame {
 			}
 		}
 	}
-
-	@Override
-	public void mousePressed(int button, int x, int y) {
-		this.mousePressed = true;
-		this.mouseX = x;
-		this.mouseY = y;
-	}
-
-	@Override
-	public void mouseReleased(int button, int x, int y) {
-		this.mousePressed = false;
-	}
-
 }
