ef54269df46b0756f0fa9a48dfa2efa46e72388f
escudero <escudero@0ed138ed-9e3e-4e1a-b415-34bd3ada5812>
2012-01-25 11:08:39 +0000

    modification nom de fonction getGainArgent --> gainArgent getGainArgent (getteur de l'attribut gainArgent) rétabli
    git-svn-id: https://www.etud.insa-toulouse.fr/svn/CredigeMegaUpload/trunk@36 0ed138ed-9e3e-4e1a-b415-34bd3ada5812

--- MegaUpload/src/org/insa/megaupload/entities/Serveur.java
+++ MegaUpload/src/org/insa/megaupload/entities/Serveur.java
@@ -4,11 +4,13 @@ import org.insa.megaupload.example.Context;
 import org.insa.megaupload.rules.ServeurRules;
 
 public class Serveur {
+	private int gainArgent;
 	private int gainCulturel;
 	private Lieu lieuHebergement;
 	
 	
 	public Serveur(int gainArgent, int gainCulturel, Lieu lieuHebergement) {
+		this.gainArgent = 0;
 		this.gainCulturel = gainCulturel;
 		this.lieuHebergement = lieuHebergement;
 		
@@ -17,10 +19,15 @@ public class Serveur {
 	}
 	
 	//déclenché tous les X base de temps, augmente le compteur de thune avec les gains du serveur
-	public void getGainArgent() {
+	public void gainArgent() {
 		Context.incCptThunes(ServeurRules.getRegleGainBase());
+		this.gainArgent += ServeurRules.getRegleGainBase();
 	}
 	
+	public int getGainArgent() {
+		return gainArgent;
+	}
+
 	public int getGainCulturel() {
 		return gainCulturel;
 	}
