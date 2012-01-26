1fe9c6d0488bcb8054e1f912fe097cd8eaec0313
escudero <escudero@0ed138ed-9e3e-4e1a-b415-34bd3ada5812>
2012-01-26 18:40:01 +0000

    paiement immédiat dès l'ouverture du serveur
    git-svn-id: https://www.etud.insa-toulouse.fr/svn/CredigeMegaUpload/trunk@88 0ed138ed-9e3e-4e1a-b415-34bd3ada5812

--- MegaUpload/src/org/insa/megaupload/actions/OuvertureServeur.java
+++ MegaUpload/src/org/insa/megaupload/actions/OuvertureServeur.java
@@ -2,11 +2,15 @@ package org.insa.megaupload.actions;
 
 import org.insa.megaupload.entities.Personnage;
 import org.insa.megaupload.entities.Serveur;
+import org.insa.megaupload.example.Context;
+import org.insa.megaupload.rules.ServeurRules;
 
 public class OuvertureServeur extends Action {
 
 	public OuvertureServeur(Personnage perso, int tempsTotal) {
 		super(perso, tempsTotal);
+		//décompte le cout d'ouverture du serveur du compteur de thune
+		Context.decCptThunes(ServeurRules.getRegleCoutOuverture());
 	}
 
 	@Override
--- MegaUpload/src/org/insa/megaupload/entities/Serveur.java
+++ MegaUpload/src/org/insa/megaupload/entities/Serveur.java
@@ -6,8 +6,6 @@ import org.insa.megaupload.rules.ServeurRules;
 public class Serveur {
 
 	public Serveur() {
-		//décompte le cout d'ouverture du serveur du compteur de thune
-		Context.decCptThunes(ServeurRules.getRegleCoutOuverture());
 	}
 	
 	//déclenché tous les X base de temps, augmente le compteur de thune avec les gains du serveur
