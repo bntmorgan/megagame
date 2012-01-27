f7a1ac91efa60fe2a18ba59a54391f27e5e69239
wernert <wernert@0ed138ed-9e3e-4e1a-b415-34bd3ada5812>
2012-01-27 14:09:45 +0000

    renommage en megagame
    git-svn-id: https://www.etud.insa-toulouse.fr/svn/CredigeMegaUpload/trunk@140 0ed138ed-9e3e-4e1a-b415-34bd3ada5812

--- MegaUpload/src/org/insa/megaupload/game/MegaUploadGame.java
+++ MegaUpload/src/org/insa/megaupload/game/MegaUploadGame.java
@@ -21,7 +21,7 @@ public class MegaUploadGame extends StateBasedGame {
 	public static void main(String argv[]) {
 		try {
 			AppGameContainer app;
-			app = new AppGameContainer(new MegaUploadGame("MegaUpload Game"));
+			app = new AppGameContainer(new MegaUploadGame("MegaGame"));
 			app.setDisplayMode(1280, 667, false);
 			app.setMaximumLogicUpdateInterval(10);
 			app.setMinimumLogicUpdateInterval(10);
